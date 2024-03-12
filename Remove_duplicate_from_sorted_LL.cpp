2542c43b62382e5e1f12884894e26639c6b41170
sumitssk <sumit.kawade20@pccoepune.org>
2024-03-12 16:17:13 +0530

    problems on singly LL

---
+++ Remove_duplicate_from_sorted_LL.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printLinkedList(Node* head)
+{
+    if(head==NULL)return ;
+    if(head->next==NULL)
+    {
+        cout<<head->data;
+        return ;
+    }
+    cout<<head->data<<"->";
+    printLinkedList(head->next);
+
+}
+Node* create_linked_list();
+
+void removeDuplicates(Node* head)
+{
+    if(head==NULL || head->next==NULL)return;
+
+    while(head!=NULL && head->next!=NULL)
+    {
+        while(head->next!=NULL && head->data==head->next->data)
+        {
+            Node* temp=head->next;
+            head->next=head->next->next;
+            delete temp;
+        }
+        head=head->next;
+    }
+}
+
+int main()
+{
+    int t;
+    cout<<"Enter the no of test cases:";
+    cin>>t;
+    while(t--)
+    {
+        Node *head=create_linked_list();
+        printLinkedList(head);
+        cout<<"\n";
+        removeDuplicates(head);
+        printLinkedList(head);
+        cout<<"\n";
+    }
+    return 0;
+}
+
+Node* create_linked_list()
+{
+    int n;
+    cout<<"Enter the el in Linked list:";
+    cin>>n;
+    if(n==0)return NULL;
+    int head_el;
+    cout<<"Enter the el at the head:";
+    cin>>head_el;
+    Node *head=new Node(head_el);
+    Node *curr=head;
+    n=n-1;
+    while(n--)
+    {
+        int el;
+        cin>>el;
+        curr->next=new Node(el);
+        curr=curr->next;
+
+    }
+    return head;
+
+}
